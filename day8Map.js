638c382b824aa5bbc97bb807f89d9f661c080686
Hajime <114803667+hajimeyudha@users.noreply.github.com>
2023-06-21 23:29:34 +0700

    edit

--- day8Map.js
+++ day8Map.js
@@ -1,10 +1,11 @@
 //map sama seperti object
 //bedanya key dari map bisa tipe data apapun, sedangkan objek hanya string/simbol
 
-const mapSaya = new Map([
-  ['1', 'a String key'],
+const mapSaya = new Map([ //first array buat menyimpan key value
+  ['1', 'a String key'],//1 adaalah key, a string key adalah value
   [1, 'a number key'],
   [true, true]
 ]);
 
 console.log(mapSaya);
+console.log(mapSaya.get('1'));
