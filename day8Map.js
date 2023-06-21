6c92cba88b50030a76b9f488b20f2dc54a1d25f0
Hajime <114803667+hajimeyudha@users.noreply.github.com>
2023-06-21 22:59:56 +0700

    map

---
+++ day8Map.js
@@ -0,0 +1,10 @@
+//map sama seperti object
+//bedanya key dari map bisa tipe data apapun, sedangkan objek hanya string/simbol
+
+const mapSaya = new Map([
+  ['1', 'a String key'],
+  [1, 'a number key'],
+  [true, true]
+]);
+
+console.log(mapSaya);
